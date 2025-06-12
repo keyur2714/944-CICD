66be280feea4b18668fb6d9195d22eb717088c5b
keyur.thakor <keyurdevops937@gmail.com>
2025-06-12 08:26:43 +0530

    initial check in of my phpweb code

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AWS Beanstalk PHP Test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Hello Radhe Krishna...!</h1>
+        <p>PHP version: <?php echo phpversion(); ?></p>
+    </div>
+</body>
+</html>
