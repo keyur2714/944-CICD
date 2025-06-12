dee4d6f0d047b7193a061c4d82168f97bf79a01a
keyur.thakor <keyurdevops937@gmail.com>
2025-06-12 08:34:50 +0530

    updated with html table

--- index.php
+++ index.php
@@ -9,6 +9,31 @@
     <div class="container">
         <h1>Hello Radhe Krishna...!</h1>
         <p>PHP version: <?php echo phpversion(); ?></p>
+		
+		<h2>User Info Table</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Alice</td>
+                    <td>25</td>
+                </tr>
+                <tr>
+                    <td>Bob</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td>Charlie</td>
+                    <td>28</td>
+                </tr>
+            </tbody>
+        </table>
+		
     </div>
 </body>
 </html>
