b1267a133214903bbd96595499ac83e93df61a17
keyur.thakor <keyurdevops937@gmail.com>
2025-06-12 08:43:54 +0530

    updated table css

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <div class="container">
         <h1>Hello Radhe Krishna...!</h1>
         <p>PHP version: <?php echo phpversion(); ?></p>
-		
-		<h2>User Info Table</h2>
+
+        <h2>User Info Table</h2>
         <table>
             <thead>
                 <tr>
@@ -19,21 +19,24 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr class="row-blue">
                     <td>Alice</td>
                     <td>25</td>
                 </tr>
-                <tr>
+                <tr class="row-grey">
                     <td>Bob</td>
                     <td>30</td>
                 </tr>
-                <tr>
+                <tr class="row-blue">
                     <td>Charlie</td>
                     <td>28</td>
                 </tr>
+                <tr class="row-grey">
+                    <td>Diana</td>
+                    <td>22</td>
+                </tr>
             </tbody>
         </table>
-		
     </div>
 </body>
 </html>
